94511dadb818c2e1dfcd17aca6964f511481cbeb
Rinay Shah <rinayshah@yahoo.com>
2018-08-06 17:08:53 -0400

    Updated

---
+++ Rinay's Credit Card Validator/WindowsFormsApplication13/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WindowsFormsApplication13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(97, 170);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Visa";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(137, 193);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Discover";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(17, 193);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(79, 17);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "MasterCard";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(17, 216);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(79, 17);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Diners Club";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(137, 216);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(109, 17);
+            this.radioButton5.TabIndex = 8;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "American Express";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(78, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(17, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::WindowsFormsApplication13.Properties.Resources.Diners_Club_ai1;
+            this.pictureBox5.Location = new System.Drawing.Point(145, 48);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 94);
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::WindowsFormsApplication13.Properties.Resources.photo1;
+            this.pictureBox4.Location = new System.Drawing.Point(142, 65);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 65);
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApplication13.Properties.Resources.discover;
+            this.pictureBox3.Location = new System.Drawing.Point(142, 54);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 84);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApplication13.Properties.Resources.mastercard_logo1;
+            this.pictureBox2.Location = new System.Drawing.Point(142, 54);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(104, 84);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication13.Properties.Resources.images;
+            this.pictureBox1.Location = new System.Drawing.Point(142, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 53);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 274);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = ",";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ Rinay's Credit Card Validator/WindowsFormsApplication13/Form1.cs
@@ -0,0 +1,246 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private string cardType(string cardnumber)
+            // Checks the type of Credit Card
+        {
+            string typeofcard = "Unknown";
+            label1.ForeColor = Color.Red;
+            if (cardnumber.Length == 16 && cardnumber.Substring(0, 1) == "4")
+            {
+                typeofcard = "Visa";
+                label1.ForeColor = Color.Green;
+            }
+
+            if (cardnumber.Length == 16 && (cardnumber.Substring(0, 2) == "51" || cardnumber.Length == 16 & cardnumber.Substring(0, 2) == "52" || cardnumber.Length == 16 & cardnumber.Substring(0, 2) == "53" || cardnumber.Length == 16 & cardnumber.Substring(0, 2) == "54" || cardnumber.Length == 16 & cardnumber.Substring(0, 2) == "55"))
+            {
+                typeofcard = "Master Card";
+                label1.ForeColor = Color.Green;
+            }
+            if (cardnumber.Length == 15 && (cardnumber.Substring(0, 2) == "34" || cardnumber.Length == 15 & cardnumber.Substring(0, 2) == "37"))
+            {
+                typeofcard = "American Express";
+                label1.ForeColor = Color.Green;
+            }
+            if (cardnumber.Length == 16 && cardnumber.Substring(0, 4) == "6011")
+            {
+                typeofcard = "Discover";
+                label1.ForeColor = Color.Green;
+            }
+            if (cardnumber.Length == 14 && cardnumber.Substring(0, 2) == "36")
+            {
+                typeofcard = "Diners Club";
+                label1.ForeColor = Color.Green;
+            }
+            return typeofcard;
+
+        }
+        private bool checkSumValid(string cardnumber)
+        {
+            if (cardnumber == "")
+                return false;
+            int sumDouble = 0;
+            int sumAdd = 0;
+            int CheckSum = 0;
+            //Starting from the back of the credit card every other number is multiplied by 2
+            for (int i = cardnumber.Length - 2; i >= 0; i -= 2)
+            {
+                int otherSum = 0;
+                int x = Convert.ToInt32(cardnumber.Substring(i, 1));
+                x = 2 * x;
+                // OtherSum Stores the first value of the number and SumDouble stores the last value number
+                do
+                {
+                    otherSum += x % 10;
+                    x = x / 10;
+                } while (x != 0);
+                sumDouble += otherSum;
+            }
+            // Adds the remaning values to the CheckSum
+            for (int y = cardnumber.Length - 1; y >= 0; y -= 2)
+            {
+                int x = Convert.ToInt32(cardnumber.Substring(y, 1));
+                sumAdd += x;
+            }
+            CheckSum = sumDouble + sumAdd;
+            if (CheckSum % 10 == 0 && cardnumber.Length <= 16)
+                return true;
+            else return false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+            //Picture Visiblity 
+        { 
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            label1.Text = cardType(textBox1.Text);
+            if (cardType(textBox1.Text) == "Visa")
+            {
+                pictureBox1.Visible = true;
+            }
+            if (cardType(textBox1.Text) == "Master Card")
+            {
+                pictureBox2.Visible = true;
+            }
+            if (cardType(textBox1.Text) == "Discover")
+            {
+                pictureBox3.Visible = true;
+            }
+            if (cardType(textBox1.Text) == "American Express")
+            {
+                pictureBox4.Visible = true;
+            }
+            if (cardType(textBox1.Text) == "Diners Club")
+            {
+                pictureBox5.Visible = true;
+            }
+            if (checkSumValid(textBox1.Text))
+            {
+                label2.Text = "Valid Number";
+                label2.ForeColor = Color.Green;
+            }
+            else
+            {
+                label2.Text = "Invalid Number";
+                label2.ForeColor = Color.Red;     
+            }
+        }
+        private string CreditCardGenerator(int prefix)
+        {
+            int length = 0;
+            int abc = 0;
+            Random ram = new Random();
+            string generaterandomnumber = prefix.ToString();
+            // Sets the credit length after a radio button is clicked 
+            if (radioButton1.Checked)
+            {
+                length = 16;
+            }
+            if (radioButton2.Checked)
+            {
+                length = 16;
+            }
+            if (radioButton3.Checked)
+            {
+                length = 16;
+            }
+            if (radioButton4.Checked)
+            {
+                length = 14;
+            }
+            if (radioButton5.Checked)
+            {
+                length = 15;
+            }
+            do
+                //Sets random numbers between 1 to 9 upto the 2nd last number in the card length
+            {
+                int digits = ram.Next(0, 10);
+                generaterandomnumber += digits.ToString();
+            } while (generaterandomnumber.Length < length - 1);
+            //Last digit for the credit card number is 0
+            generaterandomnumber += abc.ToString();
+            //CheckSum Code
+            int sumdouble = 0;
+            int sumadd = 0;
+            int checksum = 0;
+            for (int i = generaterandomnumber.Length - 2; i >= 0; i -= 2)
+            {
+                int othersum = 0;
+                int x = Convert.ToInt32(generaterandomnumber.Substring(i, 1));
+                x = 2 * x;
+                do
+                {
+                    othersum += x % 10;
+                    x = x / 10;
+                } while (x != 0);
+                sumdouble += othersum;
+            }
+            for (int y = generaterandomnumber.Length - 1; y >= 0; y -= 2)
+            {
+                int x = Convert.ToInt32(generaterandomnumber.Substring(y, 1));
+                sumadd += x;
+            }
+            checksum = sumdouble + sumadd;
+
+            if (checksum % 10 != 0)
+            {
+                //Last number which was 0 is removed and replaced by the new value of abc
+                abc = 10 - (checksum % 10);
+                generaterandomnumber = generaterandomnumber.Remove(length - 1);
+                generaterandomnumber += abc.ToString();
+            }
+            return generaterandomnumber.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int pick = r.Next(5);
+            int pick2 = r.Next(2);
+            // Picks randomly between 34 and 37 
+            if (pick2 == 0)
+            {
+                pick2 = 34;
+            }
+            else
+            {
+                pick2 = 37;
+            }
+            // The prefix of each credit card number is added
+            if (radioButton1.Checked)
+                textBox2.Text = CreditCardGenerator(4);
+            if (radioButton2.Checked)
+                textBox2.Text = CreditCardGenerator(6011);
+            if (radioButton3.Checked)
+                textBox2.Text = CreditCardGenerator(pick + 51);
+            if (radioButton4.Checked)
+                textBox2.Text = CreditCardGenerator(36);
+            if (radioButton5.Checked)
+                textBox2.Text = CreditCardGenerator(pick2);
+
+        }
+
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = false;
+            }
+        }
+    }
+}
+
+
+
+
+
+
